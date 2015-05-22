09e387630a9a9ac4af9f3d08ed154bf021e3766d
Twyxt95 <a.schmidt0101@web.de>
2015-05-22 17:40:29 +0200

    erste Methodendefinition in Spiel + Klasse Karte
    erste Version mit Methodennamen und Beschreibung

--- src/control/Spiel.java
+++ src/control/Spiel.java
@@ -6,13 +6,49 @@ package control;
  * 22.05.2015
  */
 
+import data.Holder;
+import data.Karte;
 import view.View;
+
 
 /**
  * @author adrian
  */
 public class Spiel {
+	/** Holder-Array. */
+	private Holder[] holder = new Holder[4];
+	/** Fuer den Zug aktiver Holder. */
+	private Holder activeHolder;
+	/** Speichert die View. */
+	private View view;
+	/** true solange das Spiel laeuft. */
+	private boolean isRunning = true;
+	
+	
+	
     public Spiel(View view) {
-
+       this.view=view;
+       
+       kartenInit();
+	}
+    /** Methode um den Karten einen neuen Holder zuzuweisen. */
+    public boolean move(Karte karte, Holder ziel){
+    	return true;
+    }
+    /** Karten werden erstellt. */
+    private void kartenInit(){
+    	
+    }
+    /** prüft ob der aktuelle Zug gültig ist. */
+    private boolean pruefeZug(Karte karte, Holder ziel){
+    	return true;
+    }
+    /** prüft ob die gewählte Farbe in Ordnung ist. */
+    private boolean pruefeFarbe(Karte karte, Holder ziel){
+        return true;	
+    }
+    /** prüft ob die gewählte Nummer in Ordnung ist. */
+    private boolean pruefeNummer(Karte karte, Holder ziel){
+    	return true;
     }
 }

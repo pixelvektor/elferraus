9e4c80d861b8d3830dc34b4555de083d1cebf459
LaMaSi <lara@sievers-home.de>
2015-05-29 17:49:45 +0200

    Methode mischen überarbeitet

--- src/data/Stapel.java
+++ src/data/Stapel.java
@@ -1,19 +1,26 @@
-
 package data;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Stapel extends Holder{
+	private ArrayList<Karte> karten = new ArrayList<Karte>();
+	private ArrayList<Karte> zwischenKarten = new ArrayList<Karte>();
 
 	public void mischen() {
 		         
-	         Karte tmp;
-	         Karte zufall;
-	         Random() random = new Random()
+	         Karte zwischenspeicher;
+	         int zufall;
+	         
+	         Random random = new Random();
 	           for(int i = 0; i < 79; i++) {
-	             zufall = random.nextInt(Karten.length);
-	             temporaer = Karten[i]; 
-	             Karten[i] = Karten[zufall]; 
-	             Karten[zufall] =temporaer;
-	}
+	             zufall = random.nextInt(80);
+	             zwischenspeicher = zeigeKarten().get(i); 
+	             zwischenspeicher = zeigeKarten().get(zufall); 
+	             public ArrayList<Karte> zeigeKarten().get(zufall) = zwischenspeicher;
+	           }
+    }
+	           
 
 	public Karte getObersteKarte() {
 		// TODO Auto-generated method stub

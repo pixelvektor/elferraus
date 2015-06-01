a2348dd39c4b459514009bcb907154daedf627db
Fabian <fabian.schschneider@googlemail.com>
2015-06-01 15:19:19 +0200

    Vorbereitung für Kommandozeile.

---
+++ src/view/View.java
@@ -0,0 +1,28 @@
+/** Hochschule Hamm-Lippstadt
+ * Praktikum Informatik II (ElferRaus)
+ * (C) 2015 Lara Sievers, Adrian Schmidt, Fabian Schneider
+ * 22.05.2015
+ */
+
+package view;
+
+import control.Spiel;
+
+/**
+ * @author fabian
+ *
+ */
+public class View implements ViewInterface {
+	
+
+	public View() {
+		
+	}
+	
+	@Override
+	public void update(Spiel spiel) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

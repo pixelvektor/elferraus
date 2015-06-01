435dedbaf002af22d20c0b2ca789c88b9772ba25
Fabian <fabian.schschneider@googlemail.com>
2015-06-02 00:33:33 +0200

    Grundlegender Spielaufbau für die Kommandozeile implementiert.

--- src/view/View.java
+++ src/view/View.java
@@ -6,6 +6,10 @@
 
 package view;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import control.Spiel;
 
 /**
@@ -13,16 +17,58 @@ import control.Spiel;
  *
  */
 public class View implements ViewInterface {
-	
+	private static final String HELP_MESSAGE = "Um eine Karte zu ziehen geben Sie 'pull' ein.\r\n"
+			+ "Um eine Karte zu legen geben Sie den Farbbuchstaben und die Nummer ein. Bsp: R9.\r\n"
+			+ "Und wenn Sie dann doch keine Lust mehr haben sagen Sie 'bye'.\r\n";
 
+	/** Erstellt eine neue View fuer das Spiel.
+	 * Muss an Spiel uebergeben werden.
+	 */
 	public View() {
-		
+		System.out.println("ElferRaus");
+		System.out.println(HELP_MESSAGE);
 	}
 	
 	@Override
 	public void update(Spiel spiel) {
-		// TODO Auto-generated method stub
+		System.out.println("Karten des Computers: " + spiel.getSpieler().get(1).getKarten().size() + ".");
+		
+		printSpielfeld();
+		
+		System.out.println("Sie haben " + spiel.getSpieler().get(0).getKarten().size() + " auf der Hand.");
+		
+		String input = (input("Ihr Zug: "));
+		switch (input) {
+		case "bye":
+			System.out.println("Bye Bye.");
+			spiel.exit();
+			break;
 
+		default:
+			System.out.println("Ich verstehe '" + input + "' nicht.");
+			break;
+		}
+	}
+	
+	private String input(final String message) {
+		System.out.print(message);
+		String input = "";
+		try {
+			input = new BufferedReader(new InputStreamReader(System.in)).readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return input.toLowerCase();
 	}
 
+	private void printSpielfeld() {
+		System.out.print("Blau   ");
+		System.out.println();
+		System.out.print("Rot    ");
+		System.out.println();
+		System.out.print("Gruen  ");
+		System.out.println();
+		System.out.print("Orange ");
+		System.out.println();
+	}
 }

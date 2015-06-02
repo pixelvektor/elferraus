fec6a1f8d1459cbfce965ec57a14811d1dce79cc
Fabian <fabian.schschneider@googlemail.com>
2015-06-02 23:48:33 +0200

    Ausbau der Getter für die höchste / niedrigste Karte pro Farbe.

--- src/data/Holder.java
+++ src/data/Holder.java
@@ -1,6 +1,8 @@
 package data;
 
+import java.awt.Color;
 import java.util.ArrayList;
+
 import data.Karte;
 
 /** Hochschule Hamm-Lippstadt
@@ -34,26 +36,56 @@ public abstract class Holder {
 		
 	}
 
-	public Karte GetObersteKartenGruen() {
-		// TODO Auto-generated method stub
-		return null;
+	/** Gibt die kleinste Karte unterhalb von 11 der gesuchten Farbe zurueck.
+	 * @param farbe Die Farbe (BLUE, GREEN, ORANGE, RED) fuer welche die kleinste Karte gesucht wird.
+	 * @return Die kleinste Karte der Farbe, sofern kleiner 11. Sonst null.
+	 */
+	public Karte getLowestCard(final Color farbe) {
+		Karte lowestCard = null;
+		
+		for (Karte k : karten) {
+			if (k.getFarbe().equals(farbe) && k.getNummer() < 11) {
+				// Sofern noch keine Karte gesetzt wurde passt der erste Treffer immer
+				if (lowestCard == null) {
+					lowestCard = k;
+				}
+				// Sofern eine kleinere Karte gefunden ist, wird sie als Kleinste gesetzt
+				else if (lowestCard.getNummer() > k.getNummer()) {
+					lowestCard = k;
+				}
+			}
+		}
+		
+		return lowestCard;
 	}
 
-	public Karte GetObersteKartenOrange() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public Karte GetObersteKartenRot() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public Karte getObersteKartenBlau() {
-		// TODO Auto-generated method stub
-		return null;
+	/** Gibt die hoechste Karte oberhalb von 11 der gesuchten Farbe zurueck.
+	 * @param farbe Die Farbe (BLUE, GREEN, ORANGE, RED) fuer welche die hoechste Karte gesucht wird.
+	 * @return Die hoechste Karte der Farbe, sofern groesser 11. Sonst null.
+	 */
+	public Karte getHighestCard(final Color farbe) {
+		Karte highestCard = karten.get(0);
+		
+		for (Karte k : karten) {
+			if (k.getFarbe().equals(farbe) && k.getNummer() > 11) {
+				// Sofern noch keine Karte gesetzt wurde passt der erste Treffer immer
+				if (highestCard == null) {
+					highestCard = k;
+				}
+				// Sofern eine hoehere Karte gefunden ist, wird sie als Hoechste gesetzt
+				else if (highestCard.getNummer() < k.getNummer()) {
+					highestCard = k;
+				}
+			}
+		}
+		
+		return highestCard;
 	}
 	
+	/** Nur fuer den Stapel relevant.
+	 * UNDER CONSTRUCTION!
+	 * @return
+	 */
 	public Karte getObersteKarte() {
 		return karten.get(0);
 	}

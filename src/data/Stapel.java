97caa2045abdfbdcd8e27cf7a6f423200906f0ba
Fabian <fabian.schschneider@googlemail.com>
2015-06-05 16:26:02 +0200

    getNext implementiert.

--- src/data/Stapel.java
+++ src/data/Stapel.java
@@ -8,5 +8,15 @@ public class Stapel extends Holder{
 		Collections.shuffle(this.getKarten());
 		System.out.println(this.getKarten().size());
 	}
+	
+	/** Nimmt eine Karte vom Stapel und gibt diese zurueck.
+	 * Die Karte wird auf dem Stapel geloescht.
+	 * @return Unterste Karte vom Stapel. null bei leerem Stapel.
+	 */
+	public Karte getNext() {
+		Karte karte = this.getKarten().get(0);
+		this.remove(karte);
+		return karte;
+	}
 }
 
